287afbf38e80b56badb60abcbf038b62c43a0553
Yassine MALTI <yassinemalti@gmail.com>
2017-03-20 10:31:27 +0100

    200320171031

--- app/src/main/java/com/wordpress/yassinemalti/museearthistoiretlemcen/activity/ShellService.java
+++ app/src/main/java/com/wordpress/yassinemalti/museearthistoiretlemcen/activity/ShellService.java
@@ -2,24 +2,56 @@ package com.wordpress.yassinemalti.museearthistoiretlemcen.activity;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 public class ShellService extends Service {
-    public ShellService() {
-    }
+
+    private static String TAG = "ShellService";
+    private Handler handler;
+    private Runnable runnable;
+    private final int runTime = 5000;
 
     @Override
     public void onCreate() {
         super.onCreate();
-    }
+        Log.i(TAG, "onCreate");
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, START_STICKY);
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+
+                handler.postDelayed(runnable, runTime);
+            }
+        };
+        handler.post(runnable);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
+
+    @Override
+    public void onDestroy() {
+        if (handler != null) {
+            handler.removeCallbacks(runnable);
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+        Log.i(TAG, "onStart");
+    }
+
 }
